f15814977b1d639b13853633ff1dba781f1cfa83
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-02-01 08:49:36 +0100

    Start preparing gameLogic

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const gameboard = (function (){
 
     // Should this be stored under gameboard?
     const winningCombinations = [
-        [1,2,3],[4,5,6],[7,8,9],
-        [1,4,7],[2,5,8],[3,6,9],
-        [1,5,9],[3,5,7]
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
     ];
     
     function getBoard(){
@@ -30,33 +30,40 @@ const gameboard = (function (){
         }
     }
 
-    // Should this be stored under gameboard?
+    function logBoard(){
+        console.log(`${board[0]} | ${board[1]} | ${board[2]} `)
+        console.log(`${board[3]} | ${board[4]} | ${board[5]} `)
+        console.log(`${board[6]} | ${board[7]} | ${board[8]} `)
+    }
+
     function checkWinningMove(marker){
-        winningCombinations.forEach((comb)=> {
-            let [a,b,c]=comb;
-            if(board[a]==marker && board[b]==marker && board[c]==marker){
+        for (let comb of winningCombinations) {
+            let [a, b, c] = comb;
+            if (board[a] === marker && board[b] === marker && board[c] === marker) {
                 return true;
             }
-            else{
-                return false;
-            }
-        });
+        }
+        return false;
+    }
+    
+
+    function checkStall(){
+        let freePos=0;
+        board.forEach((position) =>{
+            if(position===""){ freePos++}
+        })
+
+        return freePos;
     }
 
     return {
         getBoard, 
         resetBoard,
         placeMarker,
-        checkWinningMove
+        checkWinningMove,
+        checkStall,
+        logBoard
     };
-})();
-
-/**
- * IIFE For GameController
- */
-
-const gameController= (function (){
-
 })();
 
 /**
@@ -75,3 +82,68 @@ let playerFactory=function(playerName,marker){
         getPlayerScore
     };
 };
+
+/**
+ * IIFE For GameController
+ */
+
+const gameController= (function (
+    playerOneName = "PlayerOne",
+    playerOneMarker = "X",
+    playerTwoName = "PlayerTwo",
+    playerTwoMarker = "O"
+){
+    // Create players with the given data
+    const players =[
+        playerFactory(playerOneName,playerOneMarker),
+        playerFactory(playerTwoName,playerTwoMarker)
+    ];
+    
+    // Define first player
+    let currentPlayer = 0;
+
+    // Switch between players -- only 2 options so easy bool.
+    function nextPlayer(){
+        currentPlayer = currentPlayer ? 0 : 1 ; 
+    }
+
+    // Function to return current player
+    function getCurrPlayer(){
+        return players[currentPlayer];
+    }
+
+    function playRound(input){
+        const playerMarker = getCurrPlayer().marker;
+
+        // Place Marker
+        const placed= gameboard.placeMarker(input,playerMarker);
+
+        //Check Victory Condition
+        let gameStatus = 0;
+        if(!placed){
+            gameStatus=-1;
+        }
+        else if(gameboard.checkWinningMove(playerMarker))
+            { gameStatus= 1; }
+        else if (gameboard.checkStall()==0){
+            gameStatus =2;
+        } 
+        else {
+            gameStatus=0;
+            nextPlayer();
+            gameboard.logBoard();
+        }
+
+        return gameStatus;
+    }
+
+    return {
+        getCurrPlayer,
+        playRound
+    };
+
+})();
+
+
+console.log("Run gameController.playRound(index) to make a move");
+gameboard.logBoard();
